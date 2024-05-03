963ef446ad6a53699a09d2c51a7deb81c1ebfad0
NikitaSinko <167980820+NikitaSinko@users.noreply.github.com>
2024-05-03 17:32:36 +0600

    script.js

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-
 console.clear();
-
-
-
 const sliderProps = {
 	fill: "#0B1EDF",
 	background: "rgba(255, 255, 255, 0.214)",
 };
 
 const slider = document.querySelector(".range__slider");
-
-
 const sliderValue = document.querySelector(".length__title");
-
-
 slider.querySelector("input").addEventListener("input", event => {
 	sliderValue.setAttribute("data-length", event.target.value);
 	applyFill(event.target);
 });
-
 applyFill(slider.querySelector("input"));
-
 function applyFill(slider) {
 	const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
 	const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
@@ -34,13 +24,9 @@ const randomFunc = {
 	number: getRandomNumber,
 	symbol: getRandomSymbol,
 };
-
-
 function secureMathRandom() {
 	return window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1);
 }
-
-
 function getRandomLower() {
 	return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -54,38 +40,23 @@ function getRandomSymbol() {
 	const symbols = '~!@#$%^&*()_+{}":?><;.,';
 	return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-
 const resultEl = document.getElementById("result");
-
 const lengthEl = document.getElementById("slider");
-
-
 const uppercaseEl = document.getElementById("uppercase");
 const lowercaseEl = document.getElementById("lowercase");
 const numberEl = document.getElementById("number");
 const symbolEl = document.getElementById("symbol");
-
-
 const generateBtn = document.getElementById("generate");
-
 const copyBtn = document.getElementById("copy-btn");
-
 const resultContainer = document.querySelector(".result");
-
 const copyInfo = document.querySelector(".result__info.right");
-
 const copiedInfo = document.querySelector(".result__info.left");
 
-
 let generatedPassword = false;
-
-
 let resultContainerBound = {
 	left: resultContainer.getBoundingClientRect().left,
 	top: resultContainer.getBoundingClientRect().top,
 };
-
 resultContainer.addEventListener("mousemove", e => {
 	resultContainerBound = {
 		left: resultContainer.getBoundingClientRect().left,
@@ -107,8 +78,6 @@ window.addEventListener("resize", e => {
 		top: resultContainer.getBoundingClientRect().top,
 	};
 });
-
-
 copyBtn.addEventListener("click", () => {
 	const textarea = document.createElement("textarea");
 	const password = resultEl.innerText;
@@ -126,8 +95,6 @@ copyBtn.addEventListener("click", () => {
 	copiedInfo.style.transform = "translateY(0%)";
 	copiedInfo.style.opacity = "0.75";
 });
-
-
 generateBtn.addEventListener("click", () => {
 	const length = +lengthEl.value;
 	const hasLower = lowercaseEl.checked;
@@ -141,8 +108,6 @@ generateBtn.addEventListener("click", () => {
 	copiedInfo.style.transform = "translateY(200%)";
 	copiedInfo.style.opacity = "0";
 });
-
-
 function generatePassword(length, lower, upper, number, symbol) {
 	let generatedPassword = "";
 	const typesCount = lower + upper + number + symbol;
